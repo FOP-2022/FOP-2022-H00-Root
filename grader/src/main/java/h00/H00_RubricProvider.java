1956b2a46e0a4b7da400bfc05ce9fd80c8b19ebf
Jan Bormet <jan.bormet@bormet.info>
2021-10-18 17:28:53 +0200

    Add Tutortests

---
+++ grader/src/main/java/h00/H00_RubricProvider.java
@@ -0,0 +1,68 @@
+package h00;
+
+import org.sourcegrade.jagr.api.rubric.*;
+import org.sourcegrade.jagr.api.rubric.RubricForSubmission;
+
+@RubricForSubmission("h00")
+public class H00_RubricProvider implements RubricProvider {
+
+  public static final Criterion finalPositionCorrect = Criterion.builder()
+    .shortDescription("Die Endposition des Robotors ist immer exakt korrekt.")
+    .grader(
+      Grader.testAwareBuilder()
+        .requirePass(JUnitTestRef.ofMethod(() -> TutorTests.class.getMethod("finalPositionCorrectWithTrue")))
+        .requirePass(JUnitTestRef.ofMethod(() -> TutorTests.class.getMethod("finalPositionCorrectWithFalse")))
+        .pointsPassedMax()
+        .pointsFailedMin()
+        .build()
+    ).build();
+
+  public static final Criterion finalPositionAndDirectionCorrect = Criterion.builder()
+    .shortDescription("Die Endposition und die Blickrichtung des Robotors ist immer exakt korrekt.")
+    .grader(
+      Grader.testAwareBuilder()
+        .requirePass(JUnitTestRef.ofMethod(() -> TutorTests.class.getMethod("finalPositionAndDirectionCorrectWithTrue")))
+        .requirePass(JUnitTestRef.ofMethod(() -> TutorTests.class.getMethod("finalPositionAndDirectionCorrectWithFalse")))
+        .pointsPassedMax()
+        .pointsFailedMin()
+        .build()
+    ).build();
+
+  public static final Criterion coinPlacementDecisionCorrect = Criterion.builder()
+    .shortDescription("Es wird immer korrekt entschieden, ob eine Münze gelegt wird.")
+    .grader(
+      Grader.testAwareBuilder()
+        .requirePass(JUnitTestRef.ofMethod(() -> TutorTests.class.getMethod("coinPlacementDecisionCorrectWithTrue")))
+        .requirePass(JUnitTestRef.ofMethod(() -> TutorTests.class.getMethod("coinPlacementDecisionCorrectWithFalse")))
+        .pointsPassedMax()
+        .pointsFailedMin()
+        .build()
+    ).build();
+
+  public static final Criterion coinPositionCorrect = Criterion.builder()
+    .shortDescription("Die Münze wird an der korrekten Stelle platziert.")
+    .grader(
+      Grader.testAwareBuilder()
+        .requirePass(JUnitTestRef.ofMethod(() -> TutorTests.class.getMethod("coinPositionCorrect")))
+        .pointsPassedMax()
+        .pointsFailedMin()
+        .build()
+    ).build();
+
+  public static final Criterion H3 = Criterion.builder()
+    .shortDescription("H3 Erste Schritte mit FOPBot")
+    .addChildCriteria(finalPositionCorrect, finalPositionAndDirectionCorrect, coinPlacementDecisionCorrect, coinPositionCorrect)
+    .build();
+
+
+  public static final Rubric RUBRIC = Rubric.builder()
+    .title("H00")
+    .addChildCriteria(H3)
+    .build();
+
+
+  @Override
+  public Rubric getRubric() {
+    return RUBRIC;
+  }
+}
---
+++ grader/src/main/java/h00/TutorTests.java
@@ -0,0 +1,114 @@
+package h00;
+
+import fopbot.*;
+import org.junit.jupiter.api.Test;
+import org.sourcegrade.jagr.api.rubric.TestForSubmission;
+
+import java.util.ArrayList;
+
+import static h00.Exercise.doExercise;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestForSubmission("h00")
+public class TutorTests {
+
+  private void setupWorld() {
+    World.reset();
+    World.setSize(5, 5);
+    World.setDelay(0);
+    World.setVisible(false);
+  }
+
+  public Field getFinalState(boolean b) {
+    setupWorld();
+    doExercise(b);
+    final var states = World.getGlobalWorld().getEntityStates();
+    assertNotNull(states);
+    assertTrue(states.size() > 0);
+    return states.get(states.size() - 1);
+  }
+
+  public Robot getRobotInFinalState(final boolean b) {
+    final var endState = getFinalState(b);
+    var robots = new ArrayList<Robot>();
+    for (final var fieldEntity : endState) {
+      if (fieldEntity instanceof Robot) {
+        robots.add((Robot)fieldEntity);
+      }
+    }
+    assertEquals(1, robots.size(), "Am Ende ist nicht genau ein Roboter vorhanden");
+    return robots.get(0);
+  }
+
+  public void finalPositionCorrect(boolean b) {
+    final var r = getRobotInFinalState(b);
+    assertEquals(0, r.getX(), "X-Koordinate des Roboters inkorrekt");
+    assertEquals(0, r.getY(), "Y-Koordinate des Roboters inkorrekt");
+  }
+
+  public void finalPositionAndDirectionCorrect(boolean b) {
+    final var r = getRobotInFinalState(b);
+    assertEquals(0, r.getX(), "X-Koordinate des Roboters inkorrekt");
+    assertEquals(0, r.getY(), "Y-Koordinate des Roboters inkorrekt");
+    assertEquals(Direction.UP, r.getDirection(), "Blickrichtung des Roboters inkorrekt");
+  }
+
+
+  @Test
+  public void finalPositionCorrectWithTrue(){
+    finalPositionCorrect(true);
+  }
+
+  @Test
+  public void finalPositionCorrectWithFalse(){
+    finalPositionCorrect(false);
+  }
+
+  @Test
+  public void finalPositionAndDirectionCorrectWithTrue(){
+    finalPositionAndDirectionCorrect(true);
+  }
+
+  @Test
+  public void finalPositionAndDirectionCorrectWithFalse(){
+    finalPositionAndDirectionCorrect(false);
+  }
+
+  @Test
+  public void coinPlacementDecisionCorrectWithTrue(){
+    final var finalState = getFinalState(true);
+    var coins = new ArrayList<Coin>();
+    for (final var fieldEntity : finalState) {
+      if (fieldEntity instanceof Coin){
+        coins.add((Coin)fieldEntity);
+      }
+    }
+    assertEquals(1, coins.size(), "Anzahl der Münzen im Endzustand inkorrekt für shouldPutCoin == true");
+  }
+
+  @Test
+  public void coinPlacementDecisionCorrectWithFalse(){
+    final var finalState = getFinalState(false);
+    for (final var fieldEntity : finalState) {
+      if (fieldEntity instanceof Coin){
+        fail("Im Endzustand liegt eine Münze, obwohl shouldPutCoin == false");
+      }
+    }
+  }
+
+  @Test
+  public void coinPositionCorrect() {
+    final var finalState = getFinalState(true);
+    var coins = new ArrayList<Coin>();
+    for (final var fieldEntity : finalState) {
+      if (fieldEntity instanceof Coin){
+        coins.add((Coin)fieldEntity);
+      }
+    }
+    assertEquals(1, coins.size(), "Anzahl der Münzen im Endzustand inkorrekt für shouldPutCoin == true");
+    assertEquals(0, coins.get(0).getX(), "X-Koordinate der Münze inkorrekt");
+    assertEquals(0, coins.get(0).getY(), "Y-Koordinate der Münze inkorrekt");
+  }
+
+
+}
