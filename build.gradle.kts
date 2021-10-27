6c2a4432875c0b7dbe2cbf40bd6c2e34b0d13839
Alexander Staeding <alexanderstaeding@gmail.com>
2021-10-27 17:49:06 +0200

    Clean up gradle stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ allprojects {
     mavenCentral()
   }
   java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
     withSourcesJar()
   }
   tasks {
